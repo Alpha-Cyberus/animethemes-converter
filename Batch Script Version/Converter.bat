cc2393e07b96c75a2d53ff69a7a443689889a445
Alpha-Cyberus <ammonobrien@gmail.com>
2021-06-08 12:57:14 +0200

    Stuck on using SET method, leaving for a branch to test findstr method

--- Batch Script Version/Converter.bat
+++ Batch Script Version/Converter.bat
@@ -9,6 +9,7 @@ GOTO :eof
 REM loop all .webm vids in folder + subfolders
 REM For each, call :Rename to neaten filenames and remove duplicate versions
 REM For each, call :Convert to convert to MP3
+
 FOR /R %%v IN (".\*.webm") DO (
 	CALL :Rename %%~nv
 	CALL :Convert %%~nv
@@ -20,11 +21,25 @@ REM find all -OP*-, remove the end, remove trailing char, replace - with space
 REM find all -OP*v, delete file
 REM repeat with EP
 SETLOCAL
-IF NOT "-OP*-" ==
 
+REM does the filename contain OP and then a -?
+SET "_filename=%~1"
+SET _opep=-OP?v
+SET _test=%_filename:-OP?v=%
+SET _
+ECHO .
 
-SET "_test=%~1"
-ECHO !_test!
+REM IF NOT !_test!==!_filename! (
+REM 		ECHO "---Different"
+REM 		) ELSE (
+REM 		ECHO "---Is the same"
+REM 		)
+
+rem SET "_cities=Aberdeen, London, Edinburgh" && ECHO !_cities!
+rem SET _dummy=!_cities:London=! && ECHO !_dummy!
+
+rem IF NOT !_dummy! == !_cities! (ECHO "NOT") ELSE (ECHO "IS")
+
 
 ENDLOCAL
 EXIT /B
