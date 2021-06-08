dc9c4b5bb8f0d742d53d200b875cd53c8c5b994f
Alpha-Cyberus <ammonobrien@gmail.com>
2021-06-08 13:02:23 +0200

    Formatting update

--- Batch Script Version/Converter.bat
+++ Batch Script Version/Converter.bat
@@ -9,22 +9,14 @@ GOTO :eof
 REM loop all .webm vids in folder + subfolders
 REM For each, call :Rename to neaten filenames and remove duplicate versions
 REM For each, call :Convert to convert to MP3
-FOR /R %%v IN (".\*.webm") DO (
-	CALL :Rename %%~nv
-	CALL :Convert %%~nv
-	)
+	FOR /R %%v IN (".\*.webm") DO (
+		CALL :Rename %%~nv
+		CALL :Convert %%~nv
+		)
 EXIT /B
 
 :Rename
-REM find all -OP*-, remove the end, remove trailing char, replace - with space
-REM find all -OP*v, delete file
-REM repeat with EP
 SETLOCAL
-IF NOT "-OP*-" ==
-
-
-SET "_test=%~1"
-ECHO !_test!
 
 ENDLOCAL
 EXIT /B
