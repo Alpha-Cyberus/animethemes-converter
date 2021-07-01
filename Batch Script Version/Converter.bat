760d2f703165eda7aba5a4ad903a12e043f0ae07
Alpha-Cyberus <ammonobrien@gmail.com>
2021-07-01 18:06:38 +0200

    Core functionality complete

--- Batch Script Version/Converter.bat
+++ Batch Script Version/Converter.bat
@@ -1,6 +1,6 @@
 @ECHO on
 SETLOCAL EnableDelayedExpansion
-cd C:\Users\petad\Music\Anime
+CD C:\Users\petad\Music\Anime
 GOTO :Main
 GOTO :eof
 
@@ -10,47 +10,89 @@ SETLOCAL
 		SET "_pth=%%~dpV"
 		SET "_xtn=%%~xV"
 		SET "_fname=%%~nV"
-		CALL :Rename
+		SET /A _endloop=0
+		CALL :Rename "%%~nV"
+	)
+	FOR /R %%V IN (".\*.webm") DO (
+		SET "_pth=%%~dpV"
+		SET "_xtn=%%~xV"
+		SET "_fname=%%~nV"
 		CALL :ConvertClear "%%~nV"
 	)
+		DEL "converter.txt"
 ENDLOCAL
 PAUSE
 EXIT /B
 
 :Rename
 	ECHO %_fname% > converter.txt
-
-	SET "_findme=-.*-"
-	FOR /F %%I IN ('FINDSTR /R /C:!_findme! converter.txt') DO (
+	FOR /F %%I IN ('FINDSTR /R "OP[0-9]*- ED[0-9]*-" converter.txt') DO (
 		IF !ERRORLEVEL! EQU 0 (
-			CALL :Newfname- %_fname%
-			EXIT /B
+			CALL :Newfname1 "%~1"
 		)
 	)
-
-	SET "_findme=-.*v"
-	FOR /F %%I IN ('FINDSTR /R /C:!_findme! converter.txt') DO (
+	FOR /F %%I IN ('FINDSTR /R "OP[0-9]*v" converter.txt') DO (
 		IF !ERRORLEVEL! EQU 0 (
-			CALL :Newfnamev %_fname%
-			EXIT /B
+			CALL :Newfnamev "%~1" "OP"
+		)
+	)
+	FOR /F %%I IN ('FINDSTR /R "ED[0-9]*v" converter.txt') DO (
+		IF !ERRORLEVEL! EQU 0 (
+			CALL :Newfnamev "%~1" "ED"
+		)
+	)
+	ECHO %_fname% > converter.txt
+	rem old search string "-[a-Z][a-Z][0-9]\>"
+	FOR /F %%I IN ('FINDSTR /R /C:"-OP[0-9]\>" converter.txt') DO (
+		IF !ERRORLEVEL! EQU 0 (
+			CALL :NewfnamespOP
+		)
+	)
+	FOR /F %%I IN ('FINDSTR /R /C:"-ED[0-9]\>" converter.txt') DO (
+		IF !ERRORLEVEL! EQU 0 (
+			CALL :NewfnamespED
 		)
 	)
 EXIT /B
 
-:Newfname-
-	set "_left=%_fname:-=" & set "_right=%"
-	set "_fname=!_fname:%_right%=@@@!"
-	set "_fname=%_fname:-@@@=%"
+:Newfname1
+	ECHO "Do Case 1"
+	SET "_left=%_fname:-=" & set "_right=%"
+	SET "_fname=!_fname:%_right%=@@@!"
+	SET "_fname=%_fname:-@@@=%"
+	REN "%_pth%%~1%_xtn%" "%_fname%%_xtn%"
 EXIT /B
 
 :Newfnamev
-	set "_left=%_fname:-=" & set "_right=%"
-	set "_rleft=%_right:v=" & set "_rright=%"
-	set "_fname=!_left! !_rleft!"
-	echo !_fname!
+ ECHO "Do Case 2:%~2"
+	SET "_fname=!_fname:%~2=@@@!"
+	SET "_left=%_fname:@@@=" & set "_right=%"
+	SET "_rleft=%_right:v=" & set "_rright=%"
+	SET "_fname=%_left%%~2%_rleft%"
+	REN "%_pth%%~1%_xtn%" "%_fname%%_xtn%"
+EXIT /B
+
+:NewfnamespOP
+	ECHO "Do Case 3"
+	SET "_fnamesp=%_fname:-OP= OP%"
+	IF EXIST "%_pth%%_fnamesp%%_xtn%" (
+		DEL "%_pth%%_fname%%_xtn%"
+	) ELSE (
+		REN "%_pth%%_fname%%_xtn%" "%_fnamesp%%_xtn%"
+	)
+EXIT /B
+
+:NewfnamespED
+	ECHO "Do Case 3"
+	SET "_fnamesp=%_fname:-ED= ED%"
+	IF EXIST "%_pth%%_fnamesp%%_xtn%" (
+		DEL "%_pth%%_fname%%_xtn%"
+	) ELSE (
+		REN "%_pth%%_fname%%_xtn%" "%_fnamesp%%_xtn%"
+	)
 EXIT /B
 
 :ConvertClear
-	ffmpeg -n -i "%_pth%%~1%_xtn%" "%_pth%%_fname%.mp3"
-	DEL "%_pth%%~1%_xtn%"
+	ffmpeg -i "%_pth%%_fname%%_xtn%" "%_pth%%_fname%.mp3"
+	DEL "%_pth%%_fname%%_xtn%"
 EXIT /B
